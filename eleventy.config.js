d01631f507a827fbb59f45176ad6dba8720b41a6
dractoon <86248370+dractoon@users.noreply.github.com>
2026-09-15 16:02:44 +0100

    convert site to eleventy templates

---
+++ eleventy.config.js
@@ -0,0 +1,14 @@
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("styles.css");
+
+    return {
+        dir: {
+            input: "src",
+            includes: "../_includes",
+            output: "_site"
+        }
+    };
+
+};
